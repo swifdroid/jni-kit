de7280025bdab56bff80604cf6217e3f5cd9c917
Mikhail Isaev <isaev.mihael@gmail.com>
2025-04-20 04:17:48 +0400

    🪚 Implement `JNIKit`

---
+++ Sources/JNIKit/JNIKit.swift
@@ -0,0 +1,14 @@
+//
+//  JNIKit.swift
+//  JNIKit
+//
+//  Created by Mihael Isaev on 20.04.2025.
+//
+
+import Android
+
+public actor JNIKit: Sendable {
+    public static let shared = JNIKit()
+
+    private init() {}
+}
