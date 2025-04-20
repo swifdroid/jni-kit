61715903dc73d1a59a3f5432a0784ca434e8a4e0
Mikhail Isaev <isaev.mihael@gmail.com>
2025-04-20 14:30:23 +0400

    🪚 JNIKit: add `JVM`

--- Sources/JNIKit/JNIKit.swift
+++ Sources/JNIKit/JNIKit.swift
@@ -10,5 +10,22 @@ import Android
 public actor JNIKit: Sendable {
     public static let shared = JNIKit()
 
+    public private(set) var vm: JVM!
+
     private init() {}
+
+    /// Initialize JNIKit with the JavaVM pointer
+    public func initialize(with vm: JVM) {
+        self.vm = vm
+    }
+
+    /// Attach current thread and get a JNIEnv*
+    public func attachCurrentThread() -> JEnv? {
+        vm.attachCurrentThread()
+    }
+
+    /// Detach current thread (optional)
+    public func detachCurrentThread() {
+        vm.detachCurrentThread()
+    }
 }
