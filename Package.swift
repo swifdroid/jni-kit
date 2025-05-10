775ef47ed9a9c5d7463ddafefcca09243c519e65
Mikhail Isaev <isaev.mihael@gmail.com>
2025-05-10 05:54:20 +0400

    📦 Switch to `Android` standard library

--- Package.swift
+++ Package.swift
@@ -11,9 +11,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log", from: "1.6.2")
     ],
     targets: [
-        .target(name: "CDroidJNI"),
         .target(name: "JNIKit", dependencies: [
-            .target(name: "CDroidJNI"),
             .product(name: "Logging", package: "swift-log")
         ]),
         .testTarget(name: "JNIKitTests", dependencies: ["JNIKit"]),
