297ec4465594c7f754e8718ef9cb74eee3d0e181
Mikhail Isaev <isaev.mihael@gmail.com>
2025-05-17 07:09:26 +0400

    🪚 Implement `Bool+jboolean`

---
+++ Sources/JNIKit/Extensions/Bool+jboolean.swift
@@ -0,0 +1,18 @@
+//
+//  Bool+jboolean.swift
+//  JNIKit
+//
+//  Created by Mihael Isaev on 20.04.2025.
+//
+
+import Android
+
+extension Bool {
+    var jboolean: jboolean {
+        self ? UInt8(JNI_TRUE) : UInt8(JNI_FALSE)
+    }
+}
+
+extension jboolean {
+    var value: Bool { self == JNI_TRUE }
+}
