956adf9f0112a732717cf9a2f945e5de8206ecf2
Mikhail Isaev <isaev.mihael@gmail.com>
2025-05-18 20:27:16 +0400

    🪚 JObjectable: conform to protocols, update docs

--- Sources/JNIKit/Protocols/JObjectable.swift
+++ Sources/JNIKit/Protocols/JObjectable.swift
@@ -7,6 +7,24 @@
 
 import Android
 
-public protocol JObjectable {
-    var object: jobject { get }
+/// A composite protocol that defines a common interface for interacting with any Java object (`jobject`) in Swift.
+///
+/// Once a type conforms to `JObjectable`, it automatically gains access to all
+/// standard Java object methods through protocol extensions.
+public protocol JObjectable: JEquatable, JGetClassable, JHashable, JNotifiable, JStringable, JWaitable {
+    /// The underlying JNI object reference.
+    var ref: jobject { get }
+
+    /// The resolved class reference of the Java object.
+    var clazz: JClass { get }
+
+    /// The fully qualified class name of the object (e.g., `"java/lang/String"`).
+    var className: JClassName { get }
+}
+
+extension JObjectable {
+    /// Returns the class name of the object by accessing the `clazz.name` property.
+    ///
+    /// This provides a convenient default implementation based on the associated `clazz`.
+    public var className: JClassName { clazz.name }
 }
