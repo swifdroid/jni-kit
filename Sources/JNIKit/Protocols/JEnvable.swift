434d8e471c7e095355741b63506472a94917395d
Mikhail Isaev <isaev.mihael@gmail.com>
2025-05-24 08:07:24 +0400

    🧹 Cleanup

--- Sources/JNIKit/Protocols/JEnvable.swift
+++ Sources/JNIKit/Protocols/JEnvable.swift
@@ -5,8 +5,6 @@
 //  Created by Mihael Isaev on 23.10.2021.
 //
 
-import Android
-
 /// A protocol for types that provide access to a `JEnv` instance.
 public protocol JEnvable {
     /// The JNI environment associated with the current thread or execution context.
