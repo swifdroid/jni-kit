27fb27d5e2d13542ad05bd7c1e56ef9ce02d1fe8
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-27 04:14:38 +0400

    🪚 JObject: improve memory management

--- Sources/JNIKit/JObject.swift
+++ Sources/JNIKit/JObject.swift
@@ -5,6 +5,12 @@
 //  Created by Mihael Isaev on 13.01.2022.
 //
 
+#if JNILOGS
+#if canImport(Logging)
+import Logging
+#endif
+#endif
+
 /// A Swift wrapper around a global `jobject`, retained safely across threads and JNI calls.
 ///
 /// Use `JObject` to represent any Java object passed from or constructed in Swift.
@@ -19,40 +25,64 @@ public final class JObject: Sendable, JObjectable {
     /// Current `JObject` instance
     public var object: JObject { self }
 
+    private let isProxy: Bool
+
+    private let debuggingNote: String
+
     // MARK: - Init
 
     /// Consume existing `JObject` and glue it with the right `JClass`
-    public init(_ object: consuming JObject, _ clazz: JClass) {
+    public init(_ object: consuming JObject, _ clazz: JClass, debuggingNote: String? = nil, file: String = #file, function: String = #function,  line: Int = #line) {
         self.ref = object.ref
         self.clazz = clazz
+        self.isProxy = false
+        self.debuggingNote = debuggingNote ?? "file: \(file) function: \(function) line: \(line)"
+        #if JNILOGS
+        Logger.info("🗃️🗃️🗃️ JObject init 1 non-proxy ref: \(ref.ref) for \(clazz.name.fullName) note: \(debuggingNote)")
+        #endif
     }
     
-    /// Wrap an existing `jobject` by creating a global reference.
+    /// Wrap an existing `jobject` global reference in the box with a `JClass`.
     /// - Parameters:
-    ///   - ref: The local or global `jobject`
-    ///   - className: The Java class name in JNI format (e.g. `"java/lang/String"`)
-    ///   - clazz: The resolved `JClass` wrapper
-    public init(_ ref: consuming JObjectBox, _ clazz: JClass) {
+    ///   - ref: The global `jobject` in the box
+    ///   - clazz: The Java class object representing the type of this `jobject`
+    ///   - proxy: Mark it `true` to avoid deleting the reference on deinit
+    public init(_ ref: JObjectBox, _ clazz: JClass, proxy: Bool = false, debuggingNote: String? = nil, file: String = #file, function: String = #function,  line: Int = #line) {
         self.ref = ref
         self.clazz = clazz
+        self.isProxy = proxy
+        self.debuggingNote = debuggingNote ?? "file: \(file) function: \(function) line: \(line)"
+        #if JNILOGS
+        Logger.info("🗃️🗃️🗃️ JObject init 2 non-proxy ref: \(ref.ref) for \(clazz.name.fullName) note: \(debuggingNote)")
+        #endif
     }
 
     /// Convenient overload for optional `ref` and `clazz`
-    public init?(_ ref: consuming JObjectBox?, _ clazz: JClass?) {
+    public init?(_ ref: consuming JObjectBox?, _ clazz: JClass?, debuggingNote: String? = nil, file: String = #file, function: String = #function,  line: Int = #line) {
         guard let ref, let clazz else { return nil }
         self.ref = ref
         self.clazz = clazz
+        self.isProxy = false
+        self.debuggingNote = debuggingNote ?? "file: \(file) function: \(function) line: \(line)"
+        #if JNILOGS
+        Logger.info("🗃️🗃️🗃️ JObject init 3 non-proxy ref: \(ref.ref) for \(clazz.name.fullName) note: \(debuggingNote)")
+        #endif
     }
 
     /// Private initializer to create a proxy object with a different class. Useful for casting.
-    private init(original object: JObject, _ clazz: JClass) {
+    private init(original object: JObject, _ clazz: JClass, isProxy: Bool, debuggingNote: String? = nil, file: String = #file, function: String = #function,  line: Int = #line) {
         self.ref = object.ref
         self.clazz = clazz
+        self.isProxy = isProxy
+        self.debuggingNote = debuggingNote ?? "file: \(file) function: \(function) line: \(line)"
+        #if JNILOGS
+        Logger.info("🗃️🗃️🗃️ JObject init 4 proxy ref: \(ref.ref) for \(clazz.name.fullName) note: \(debuggingNote)")
+        #endif
     }
 
     /// Returns same object reference but with the different class.
     public func cast(to className: JClassName) -> JObject? {
         guard let clazz = JClass.load(className) else { return nil }
-        return JObject(original: self, clazz)
+        return JObject(original: self, clazz, isProxy: true, debuggingNote: debuggingNote)
     }
 }
