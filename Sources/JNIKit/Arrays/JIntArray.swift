32e66e50fed3b6f9a2ee1271cc03ef74c42717ea
Mikhail Isaev <isaev.mihael@gmail.com>
2025-10-14 04:05:40 +0400

    🪚 Implement `JInt32Array`

---
+++ Sources/JNIKit/Arrays/JIntArray.swift
@@ -0,0 +1,90 @@
+//
+//  JIntArray.swift
+//  JNIKit
+//
+//  Created by Mihael Isaev on 25.09.2025.
+//
+
+#if os(Android)
+import Android
+#endif
+#if JNILOGS
+#if canImport(Logging)
+import Logging
+#endif
+#endif
+
+public typealias JIntArray = JInt32Array
+
+/// A Swift wrapper for a Java int array (`jintArray`).
+///
+/// Retains a global reference and provides access to individual `jint`s.
+public final class JInt32Array: Java1DArrayable {
+    public typealias ElementType = Int32
+
+    public let object: JObject
+    public let length: Int
+
+    public init (_ object: JObject, length: Int) {
+        self.object = object
+        self.length = length
+    }
+
+    #if os(Android)
+    public static func newArrayObject(_ env: JEnv, _ length: Int) -> jobject? {
+        env.newIntArray(length: Int32(length))
+    }
+    #endif
+
+    public static func setArrayRegion<C>(_ env: JEnv, _ object: JObject, _ collection: C) where C : ToJavaArrayIterable, ElementType == C.Element {
+        #if os(Android)
+        var cArray: [ElementType] = collection.array ?? collection.makeIterator().map { $0 }
+        env.setIntArrayRegion(object.ref.ref, start: 0, length: jsize(collection.count), buffer: &cArray)
+        #endif
+    }
+
+    public static func getElementAtIndex(_ index: Int, _ object: JObject) -> ElementType? {
+        #if os(Android)
+        guard let env = JEnv.current() else { return nil }
+        var result: ElementType = 0
+        env.getIntArrayRegion(object.ref.ref, start: jsize(index), length: 1, buffer: &result)
+        return result
+        #else
+        return nil
+        #endif
+    }
+
+    public static func getAllElements(_ env: JEnv, _ object: JObject, _ length: Int) -> [ElementType] {
+        #if os(Android)
+        var result = [ElementType](repeating: 0, count: length)
+        env.getIntArrayRegion(object.ref.ref, start: 0, length: Int32(length), buffer: &result)
+        return result
+        #else
+        return []
+        #endif
+    }
+}
+
+public typealias JIntArray2D = JInt32Array2D
+
+public final class JInt32Array2D: Java2DArrayable {
+    public typealias ElementType = Int32
+
+    public let object: JObject
+    public let length: Int
+
+    public static var elementClass: JClassName { "[I" }
+    
+    public init (_ object: JObject, length: Int) {
+        self.object = object
+        self.length = length
+    }
+
+    public static func javaArrayFromCollection<C: ToJavaArrayIterable>(_ collection: C) -> JObject? where C.Element == ElementType {
+        JInt32Array(collection)?.object
+    }
+
+    public static func arrayFromJavaArray(_ env: JEnv, _ obj: JObject) -> [ElementType]? {
+        JInt32Array(env, obj).toArray()
+    }
+}
