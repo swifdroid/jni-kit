efc023a89107ec15f649155f465d2fe30c9b9483
Mikhail Isaev <isaev.mihael@gmail.com>
2025-10-17 04:01:43 +0400

    🪚 Implement `JInt8Array`

---
+++ Sources/JNIKit/Arrays/JByteArray.swift
@@ -0,0 +1,85 @@
+//
+//  JByteArray.swift
+//  JNIKit
+//
+//  Created by Mihael Isaev on 25.09.2025.
+//
+
+#if os(Android)
+import Android
+#endif
+
+public typealias JByteArray = JInt8Array
+
+/// A Swift wrapper for a Java byte array (`jbyteArray`).
+///
+/// Retains a global reference and provides access to individual `jbyte`s.
+public final class JInt8Array: Java1DArrayable {
+    public typealias ElementType = Int8
+
+    public let object: JObject
+    public let length: Int
+
+    public init (_ object: JObject, length: Int) {
+        self.object = object
+        self.length = length
+    }
+
+    #if os(Android)
+    public static func newArrayObject(_ env: JEnv, _ length: Int) -> jobject? {
+        env.newByteArray(length: Int32(length))
+    }
+    #endif
+
+    public static func setArrayRegion<C>(_ env: JEnv, _ object: JObject, _ collection: C) where C : ToJavaArrayIterable, ElementType == C.Element {
+        #if os(Android)
+        var cArray: [ElementType] = collection.array ?? collection.makeIterator().map { $0 }
+        env.setByteArrayRegion(object.ref.ref, start: 0, length: jsize(collection.count), buffer: &cArray)
+        #endif
+    }
+
+    public static func getElementAtIndex(_ index: Int, _ object: JObject) -> ElementType? {
+        #if os(Android)
+        guard let env = JEnv.current() else { return nil }
+        var result: ElementType = 0
+        env.getByteArrayRegion(object.ref.ref, start: jsize(index), length: 1, buffer: &result)
+        return result
+        #else
+        return nil
+        #endif
+    }
+
+    public static func getAllElements(_ env: JEnv, _ object: JObject, _ length: Int) -> [ElementType] {
+        #if os(Android)
+        var result = [ElementType](repeating: 0, count: length)
+        env.getByteArrayRegion(object.ref.ref, start: 0, length: Int32(length), buffer: &result)
+        return result
+        #else
+        return []
+        #endif
+    }
+}
+
+public typealias JByteArray2D = JInt8Array2D
+
+public final class JInt8Array2D: Java2DArrayable {
+    public typealias ElementType = Int8
+
+    public let object: JObject
+    public let length: Int
+
+    public static var elementClass: JClassName { "[B" }
+
+    public init (_ object: JObject, length: Int) {
+        self.object = object
+        self.length = length
+    }
+
+    public static func javaArrayFromCollection<C: ToJavaArrayIterable>(_ collection: C) -> JObject? where C.Element == ElementType {
+        JInt8Array(collection)?.object
+    }
+
+    public static func arrayFromJavaArray(_ env: JEnv, _ obj: JObject) -> [ElementType]? {
+        JInt8Array(env, obj).toArray()
+    }
+}
